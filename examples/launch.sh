1f5de372825d1e995f1f2ee0c212ae2183554638
Mohamed <mohamed@boulo.ch>
2020-06-25 12:40:13 +0000

    adds a script to launch all examples

---
+++ examples/launch.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/sh
+FILES=(*.p)
+zenonc="znmh"
+#zenonc="zenon_modulo -modulo -modulo-heuri -itptp"
+card=${#FILES[*]}
+fails=0
+success=0
+timeout="30s"
+
+
+echo "$card files to check."
+ 
+for ((j=0; j < card; j++))
+do
+	i=${FILES[$j]}
+	if $zenonc -max-time $timeout $i > /dev/null 2>&1; then
+		((success++))
+	else
+		((fails++))
+		if [[ "$1" == "-d" ]]; then
+			echo $i 
+		fi
+	fi
+	echo -ne "success: $success/$card, fails: $fails/$card\r"
+done
+echo ""
+echo "FINISHED"
+
+
